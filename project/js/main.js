cbb7b5ec7a8c585fc4cecbdb93de28ed1e070d2d
Viktor <78749896+RViktor03@users.noreply.github.com>
2022-02-19 21:06:37 +0100

    Add files via upload

---
+++ project/js/main.js
@@ -0,0 +1,6 @@
+var kepek=["nagykep/Webp.net-resizeimage.jpg","nagykep/Webp.net-resizeimage(1).jpg","nagykep/Webp.net-resizeimage(2).jpg","nagykep/Webp.net-resizeimage(3).jpg"];	
+function csere(kepnev){
+//document.nagykep.src=kepnev;
+//document.getElementById("nagykep").src=kepnev;
+document.querySelector("#nagykep").src=kepnev;
+}
